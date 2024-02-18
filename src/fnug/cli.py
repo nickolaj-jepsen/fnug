e59dc90d9daeebd32f7de5f7bc3703cdd393a120
Nickolaj Jepsen <nij@billigvvs.dk>
2024-02-18 14:23:55 +0100

    feat: add version option

--- src/fnug/cli.py
+++ src/fnug/cli.py
@@ -11,6 +11,7 @@ DEFAULT_FILE_NAMES = [".fnug.json", ".fnug.yaml", ".fnug.yml"]
 @click.command()
 @click.option("--config", "-c", type=click.Path(), help="Config file")
 @click.option("--verbose", "-v", is_flag=True, help="Verbose output")
+@click.version_option()
 def cli(config: str | None = None, verbose: bool = False) -> None:
     """Entrypoint for the fnug CLI."""
     try:
