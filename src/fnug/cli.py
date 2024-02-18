c7753d4484f93492f7c8b84d3f30e393dfd522ef
Nickolaj Jepsen <nij@billigvvs.dk>
2024-02-18 14:22:57 +0100

    feat: slightly improved error handling

--- src/fnug/cli.py
+++ src/fnug/cli.py
@@ -10,17 +10,23 @@ DEFAULT_FILE_NAMES = [".fnug.json", ".fnug.yaml", ".fnug.yml"]
 
 @click.command()
 @click.option("--config", "-c", type=click.Path(), help="Config file")
-def cli(config: str | None = None):
+@click.option("--verbose", "-v", is_flag=True, help="Verbose output")
+def cli(config: str | None = None, verbose: bool = False) -> None:
     """Entrypoint for the fnug CLI."""
-    if config is None:
-        for file_name in DEFAULT_FILE_NAMES:
-            if Path(file_name).exists():
-                config = file_name
-                break
-        else:
-            raise click.ClickException(f"Could not find a config file. Tried: {', '.join(DEFAULT_FILE_NAMES)}")
+    try:
+        if config is None:
+            for file_name in DEFAULT_FILE_NAMES:
+                if Path(file_name).exists():
+                    config = file_name
+                    break
+            else:
+                raise click.ClickException(f"Could not find a config file. Tried: {', '.join(DEFAULT_FILE_NAMES)}")
 
-    file_path = Path(config)
-    cwd = file_path.parent
-    cfg = load_config(file_path)
-    FnugApp(cfg, cwd=cwd).run()
+        file_path = Path(config)
+        cwd = file_path.parent
+        cfg = load_config(file_path)
+        FnugApp(cfg, cwd=cwd).run()
+    except Exception as e:
+        if verbose:
+            raise
+        raise click.ClickException(str(e)) from e
