5bd3957b70eb9abc2c27384f7150d4fc5b1f49c2
Nickolaj Jepsen <nij@billigvvs.dk>
2024-11-22 18:44:10 +0100

    chore: improve logger

--- src/fnug/logging.py
+++ src/fnug/logging.py
@@ -3,10 +3,15 @@ import os
 from collections.abc import Callable
 from datetime import datetime
 from enum import IntEnum
-from typing import Any
+from inspect import Traceback
+from typing import TYPE_CHECKING, Any, Optional
 
 import click
-from textual.logging import TextualHandler
+from textual._context import active_app
+from textual._log import LogGroup, LogVerbosity
+
+if TYPE_CHECKING:
+    from textual_dev.client import DevtoolsClient
 
 LEVEL_COLORS: dict[str, Callable[[str], str]] = {
     "DEBUG": lambda x: click.style(x, fg="blue"),
@@ -17,9 +22,56 @@ LEVEL_COLORS: dict[str, Callable[[str], str]] = {
 }
 
 
-class _ColoredFormatter(logging.Formatter):
-    def format(self, record: logging.LogRecord) -> str:
-        # Save original values
+class LogHandler(logging.Handler):
+    """A Logging handler usable in both Textual and non-Textual environments."""
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    _devtools: Optional["DevtoolsClient"] = None
+
+    def _textual_devtools(self) -> Optional["DevtoolsClient"]:
+        if not self._devtools:
+            try:
+                app = active_app.get()
+            except LookupError:
+                return None
+            self._devtools = app.devtools
+
+        if self._devtools is None or not self._devtools.is_connected:
+            return None
+
+        return self._devtools
+
+    def emit(self, record: logging.LogRecord) -> None:
+        """Emit a log record."""
+        if devtools := self._textual_devtools():
+            self._textual_handler(record, devtools)
+        else:
+            self._stderr_handler(record)
+
+    def _textual_handler(self, record: logging.LogRecord, devtools: "DevtoolsClient") -> None:
+        from textual_dev.client import DevtoolsLog
+
+        terminal_width = devtools.console.width
+
+        left_header = f"[{record.levelname}] {record.name}"
+        right_header = (
+            f"{record.module}.{record.funcName}" + "[PYTHON]" if record.filename.endswith(".py") else "[RUST]"
+        )
+        spacer = " " * (terminal_width - len(left_header) - len(right_header))
+        header = f"{left_header}{spacer}{right_header}"
+
+        log = DevtoolsLog(
+            objects_or_string=(header, " " * terminal_width, record.getMessage(), "\n"),
+            caller=Traceback(record.filename, record.lineno, record.funcName, None, None),
+        )
+
+        devtools.log(
+            log, LogGroup.LOGGING, LogVerbosity.HIGH if record.levelno < logging.WARNING else LogVerbosity.HIGH
+        )
+
+    def _stderr_handler(self, record: logging.LogRecord) -> None:
         level_name = record.levelname
         message = record.msg
 
@@ -37,9 +89,9 @@ class _ColoredFormatter(logging.Formatter):
 
         if record.exc_info:
             # If there's an exception, add it with proper indentation
-            formatted_message = f"{formatted_message}\n{self.formatException(record.exc_info)}"
+            formatted_message = f"{formatted_message}\n{record.exc_info}"
 
-        return formatted_message
+        click.echo(formatted_message, err=True)
 
 
 class LogLevel(IntEnum):
@@ -74,11 +126,7 @@ def log_level_callback(_: Any, __: Any, value: str | None):
 
 def setup_logging() -> None:
     """Initialize logging."""
-    handler = TextualHandler()
-    handler.setFormatter(_ColoredFormatter())
-    logging.basicConfig(
-        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=LogLevel.from_env(), handlers=[handler]
-    )
+    logging.basicConfig(level=LogLevel.from_env(), handlers=[LogHandler()])
 
 
 def get_logger(name: str = "fnug.cli") -> logging.Logger:
