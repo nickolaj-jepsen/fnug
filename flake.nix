e5063f278392b0b7eba52556c5fdfbd008b1dc51
Nickolaj Jepsen <nickolaj@fireproof.website>
2026-02-22 23:02:16 +0100

    chore: update nix config

--- flake.nix
+++ flake.nix
@@ -14,47 +14,62 @@
     inputs.flake-parts.lib.mkFlake {inherit inputs;} {
       systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
 
+      flake.overlays.default = final: prev: {
+        fnug = final.callPackage ({
+          lib,
+          rustPlatform,
+          pkg-config,
+          cmake,
+          openssl,
+        }: let
+          cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+        in
+          rustPlatform.buildRustPackage {
+            pname = "fnug";
+            inherit (cargoToml.package) version;
+            src = ./.;
+
+            cargoLock = {
+              lockFile = ./Cargo.lock;
+              outputHashes = {};
+            };
+
+            doCheck = false;
+
+            postPatch = ''
+              cp -r vendor/vt100 $cargoDepsCopy/fnug-vt100-0.15.2
+            '';
+
+            nativeBuildInputs = [pkg-config cmake];
+            buildInputs = [openssl];
+
+            meta = {
+              description = "A nice lint runner";
+              inherit (cargoToml.package) homepage;
+              license = lib.licenses.mit;
+              mainProgram = "fnug";
+            };
+          }) {};
+      };
+
       perSystem = {
         self',
+        config,
         pkgs,
         system,
         ...
       }: let
         rustToolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
-        cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
       in {
         _module.args.pkgs = import inputs.nixpkgs {
           inherit system;
-          overlays = [(import inputs.rust-overlay)];
-        };
-
-        packages.default = pkgs.rustPlatform.buildRustPackage {
-          pname = "fnug";
-          inherit (cargoToml.package) version;
-          src = ./.;
-
-          cargoLock = {
-            lockFile = ./Cargo.lock;
-            # Vendored path dependency — tell Nix to use the local source
-            outputHashes = {};
-          };
-
-          # Test uses /root which doesn't exist in the Nix sandbox
-          doCheck = false;
-
-          postPatch = ''
-            cp -r vendor/vt100 $cargoDepsCopy/fnug-vt100-0.15.2
-          '';
-
-          nativeBuildInputs = with pkgs; [
-            pkg-config
-            cmake
-          ];
-
-          buildInputs = with pkgs; [
-            openssl
+          overlays = [
+            (import inputs.rust-overlay)
+            inputs.self.overlays.default
           ];
         };
+
+        packages.default = pkgs.fnug;
 
         devShells.default = pkgs.mkShell {
           inputsFrom = [self'.packages.default];
